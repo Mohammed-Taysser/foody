76d0d340a0c23f853ee1147b5f078e221e986ff1
mohammed-taysser <mohamedtaysser983@gmail.com>
2025-05-17 11:43:54 +0300

    chore: setup folder structure and path aliases

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import app from './app';
-import CONFIG from './config/env';
+import CONFIG from '@/config/env';
+import app from '@/app';
 
 app.listen(CONFIG.PORT, () => {
   console.log(`🚀 Server is running at http://localhost:${CONFIG.PORT}`);
