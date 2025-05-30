2fd593e2f784b814c6d91f6c1f8659e28cb2080c
mohammed-taysser <mohamedtaysser983@gmail.com>
2025-05-31 00:25:57 +0300

    feat(git-hooks): install Commitlint & commitizen

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import app from '@/app';
 import CONFIG from '@/config/env';
 
 app.listen(CONFIG.PORT, () => {
-  console.log(`🚀 Server is running at http://localhost:${CONFIG.PORT}`);
-  console.log(`📝 Docs available at http://localhost:${CONFIG.PORT}/docs`);
+  console.log(`\n🚀 Server is running at http://localhost:${CONFIG.PORT}`);
+  console.log(`📝 Docs available at http://localhost:${CONFIG.PORT}/docs\n`);
 });
